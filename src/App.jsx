644920acf076c52e92d6e44701224e46e53fd63d
Derrick Hodges <hodgesd@gmail.com>
2022-10-19 07:05:28 -0400

    mapped multiple buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import "./App.css";
 import ButtonCounter from "./components/ButtonCounter";
 
+const buttons = [
+  {
+    txt: "Button 1",
+  },
+  {
+    txt: "Button 2",
+  },
+  {
+    txt: "Button 3",
+  },
+];
+
 function App() {
   return (
     <>
       <h1 className="mt-8 text-center uppercase text-red-500">
         Button Counter
       </h1>
-
-      <ButtonCounter />
-      <ButtonCounter />
-      <ButtonCounter />
-      <ButtonCounter />
-      <ButtonCounter />
-      <ButtonCounter />
+      {buttons.map((button) => (
+        <ButtonCounter txt={button.txt} />
+      ))}
     </>
   );
 }
